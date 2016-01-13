a88262f2c7bf756d5dc8829eb26b6615f3159c95
martin pet <kontakt@martinpettersson.se>
2016-01-13 20:18:53 +0100

    Assertions

--- Pathfinding.cpp
+++ Pathfinding.cpp
@@ -10,6 +10,7 @@
 #include <tuple>
 #include <algorithm>
 #include <functional>   // std::greater
+#include <cassert> //assertions
 
 using std::unordered_map;
 using std::unordered_set;
@@ -271,23 +272,30 @@ int FindPath(const int nStartX, const int nStartY,
 		}
 	}
 
-
 	draw_grid(grid, 3, nullptr, nullptr, &path);
 	std::cout << "-------------------" << std::endl;
-
-
-
 	int shortestLength = path.size() - 1;
 	return shortestLength;
 }
 
 int main() {	
 
-	unsigned char pMap[] = { 0, 0, 1, 0, 1, 1, 1, 0, 1 };
-	int pOutBuffer[7];
-	int result = FindPath(2, 0, 0, 2, pMap, 3, 3, pOutBuffer, 7);
-	std::cout << result << std::endl;
+	unsigned char pMap[] = {
+		1, 1, 1, 1,
+		0, 1, 0, 1,
+		0, 1, 1, 1
+	};
+	int pOutBuffer[12];
+
+	int nRes = FindPath(0, 0, 1, 2, pMap, 4, 3, pOutBuffer, 12);
+	assert(nRes == 3);
+
+	assert(pOutBuffer[0] == 1);
+	assert(pOutBuffer[1] == 5);
+	assert(pOutBuffer[2] == 9);
+	std::cout << nRes << std::endl;
 
 	std::getchar();
+
 	return 0;
 }
