c65a6a0a95bf0818d78622d41004b98713f96bb2
martin pet <kontakt@martinpettersson.se>
2016-01-14 10:24:21 +0100

    More cleanup

--- Pathfinding.cpp
+++ Pathfinding.cpp
@@ -24,7 +24,6 @@ using std::tie;
 using std::string;
 
 namespace std {
-	// I know, this is technically not allowed
 	template <>
 	struct hash<tuple<int, int> > {
 		inline size_t operator()(const tuple<int, int>& location) const {
@@ -33,49 +32,6 @@ namespace std {
 			return x * 1812433253 + y;
 		}
 	};
-}
-
-std::basic_iostream<char>::basic_ostream& operator<<(std::basic_iostream<char>::basic_ostream& out, tuple<int, int> loc) {
-	int x, y;
-	tie(x, y) = loc;
-	out << '(' << x << ',' << y << ')';
-	return out;
-}
-
-template<class Graph>
-void draw_grid(const Graph& graph, int field_width,
-	unordered_map<typename Graph::Location, int>* distances = nullptr,
-	unordered_map<typename Graph::Location, typename Graph::Location>* point_to = nullptr,
-	vector<typename Graph::Location>* path = nullptr) {
-	for (int y = 0; y != graph.height; ++y) {
-		for (int x = 0; x != graph.width; ++x) {
-			typename Graph::Location id{ x, y };
-			std::cout << std::left << std::setw(field_width);
-			if (graph.walls.count(id)) {
-				std::cout << '#';
-			}
-			else if (point_to != nullptr && point_to->count(id)) {
-				int x2, y2;
-				tie(x2, y2) = (*point_to)[id];
-				// TODO: how do I get setw to work with utf8?
-				if (x2 == x + 1) { std::cout << ">"; }
-				else if (x2 == x - 1) { std::cout << "<"; }
-				else if (y2 == y + 1) { std::cout << "^"; }
-				else if (y2 == y - 1) { std::cout << "v"; }
-				else { std::cout << "* "; }
-			}
-			else if (distances != nullptr && distances->count(id)) {
-				std::cout << (*distances)[id];
-			}
-			else if (path != nullptr && find(path->begin(), path->end(), id) != path->end()) {
-				std::cout << '@';
-			}
-			else {
-				std::cout << '.';
-			}
-		}
-		std::cout << std::endl;
-	}
 }
 
 template<typename L>
@@ -93,9 +49,6 @@ template<typename T, typename Number = int>
 struct PriorityQueue {
 	typedef pair<Number, T> PQElement;
 	priority_queue<PQElement, vector<PQElement>, std::greater<PQElement> > elements;
-
-	
-
 	inline bool empty() { return elements.empty(); }
 
 	inline void put(T item, Number priority) {
@@ -109,23 +62,7 @@ struct PriorityQueue {
 	}
 };
 
-template<typename Location>
-vector<Location> reconstruct_path(
-	Location start,
-	Location goal,
-	unordered_map<Location, Location>& came_from
-	) {
-	vector<Location> path;
-	Location current = goal;
-	path.push_back(current);
-	while (current != start) {
-		current = came_from[current];
-		
-		path.push_back(current);
-	}
-	std::reverse(path.begin(), path.end());
-	return path;
-}
+
 
 struct twoDimensionalGrid {
 	typedef tuple<int, int> Location;
@@ -155,9 +92,8 @@ struct twoDimensionalGrid {
 		int x, y, dx, dy;
 		tie(x, y) = id;
 		vector<Location> results;
-		static array<Location, 4> DIRS { Location{ 1, 0 }, Location{ 0, -1 }, Location{ -1, 0 }, Location{ 0, 1 } };
-		for (auto dir : DIRS) {
-			
+		static array<Location, 4> DIRECTIONS { Location{ 1, 0 }, Location{ 0, -1 }, Location{ -1, 0 }, Location{ 0, 1 } };
+		for (auto dir : DIRECTIONS) {
 			tie(dx, dy) = dir;
 			Location next(x + dx, y + dy);
 			if (in_bounds(next) && passable(next)) {
@@ -166,19 +102,10 @@ struct twoDimensionalGrid {
 		}
 
 		if ((x + y) % 2 == 0) {
-			// aesthetic improvement on square grids
 			std::reverse(results.begin(), results.end());
 		}
 
 		return results;
-	}
-};
-
-struct GridWithWeights : twoDimensionalGrid {
-	unordered_set<Location> forests;
-	GridWithWeights(int w, int h) : twoDimensionalGrid(w, h) {}
-	int cost(Location a, Location b) {
-		return forests.count(b) ? 5 : 1;
 	}
 };
 
@@ -190,12 +117,9 @@ inline int heuristic(twoDimensionalGrid::Location a, twoDimensionalGrid::Locatio
 }
 
 template<typename Graph>
-void a_star_search
-(Graph graph,
-	typename Graph::Location start,
-	typename Graph::Location goal,
-	unordered_map<typename Graph::Location, typename Graph::Location>& came_from,
-	unordered_map<typename Graph::Location, int>& cost_so_far)
+void a_star_search (Graph graph, typename Graph::Location start, typename Graph::Location goal,
+					unordered_map<typename Graph::Location, typename Graph::Location>& came_from,
+					unordered_map<typename Graph::Location, int>& cost_so_far)
 {
 	typedef typename Graph::Location Location;
 	PriorityQueue<Location> frontier;
@@ -204,7 +128,7 @@ void a_star_search
 	cost_so_far[start] = 0;
 	while (!frontier.empty()) {
 		auto current = frontier.get();
-
+		
 		if (current == goal) {
 			break;
 		}
@@ -219,6 +143,23 @@ void a_star_search
 			}
 		}
 	}
+}
+
+template<typename Location>
+vector<Location> reconstruct_path(
+	Location start,
+	Location goal,
+	unordered_map<Location, Location>& came_from
+	) {
+		vector<Location> path;
+		Location current = goal;
+		path.push_back(current);
+		while (current != start) {
+			current = came_from[current];
+			path.push_back(current);
+	}
+	std::reverse(path.begin(), path.end());
+	return path;
 }
 
 int FindPath(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY,
